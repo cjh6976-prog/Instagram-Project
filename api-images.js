4f1edc7b6c3851349e97309a03261c60da69a29e
cjh6976-prog <cjh6976@psu.edu>
2026-03-29 21:04:00 -0400

    Instagram-Project check in 2

--- api-images.js
+++ api-images.js
@@ -1,10 +1,13 @@
 import fs from "fs";
 import path from "path";
-
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), "image.json");
-  const jsonData = fs.readFileSync(filePath, "utf-8");
-  const data = JSON.parse(jsonData);
+  try {
+    const filePath = path.join(process.cwd(), "image.json"); 
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(jsonData);
 
-  res.status(200).json(data);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to load images" });
+  }
 }
